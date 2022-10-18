0a757d922c6ed2958e9f1d3b5dda57724557c6ac
Albebot <albertotrujillocarballo@gmail.com>
2022-10-18 20:29:47 +0100

    first commit
    Primer estado del Proyecto

---
+++ backend/src/test/java/com/TCI/charlas/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TCI.charlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
